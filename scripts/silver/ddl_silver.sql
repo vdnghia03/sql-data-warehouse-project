03aa02f6f56774e4bbaf4996de82175e41e492cc
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-04 16:13:27 +0700

    Update ddl_silver.sql at TYPE of crm_prd_info columns

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -31,12 +31,13 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT
+	, cat_id NVARCHAR(50)
 	, prd_key NVARCHAR(50)
 	, prd_nm NVARCHAR(50)
 	, prd_cost INT
 	, prd_line NVARCHAR(50)
-	, prd_start_dt DATETIME
-	, prd_end_dt DATETIME
+	, prd_start_dt DATE
+	, prd_end_dt DATE
 	, dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
