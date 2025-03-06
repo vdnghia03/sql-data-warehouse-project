d7b7bd76508d9757cafbb014defc0c5511c75f57
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-06 16:20:23 +0700

    Add Sales Details Check (No complete)

--- scripts/silver/bronze_quality_checks.sql
+++ scripts/silver/bronze_quality_checks.sql
@@ -88,3 +88,68 @@ SELECT
 	, LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1  AS prd_end_dt
 FROM bronze.crm_prd_info
 WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
+
+
+-----------------------------
+-- bronze.crm_sales_details
+-----------------------------
+
+-- Check Unwanted Space
+-- Expected: No result
+SELECT
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, sls_order_dt
+	, sls_ship_dt
+	, sls_due_dt
+	, sls_sales
+	, sls_quantity
+	, sls_price
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+
+-- Check Forein Key is issue
+-- Expected: No Result
+SELECT
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, sls_order_dt
+	, sls_ship_dt
+	, sls_due_dt
+	, sls_sales
+	, sls_quantity
+	, sls_price
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info)
+
+SELECT
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, sls_order_dt
+	, sls_ship_dt
+	, sls_due_dt
+	, sls_sales
+	, sls_quantity
+	, sls_price
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
+
+-- Check Invalid Dates
+SELECT 
+	NULLIF(sls_ship_dt,0) AS sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0  
+OR LEN(sls_ship_dt) < 8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
+
+-- Check for Invalid Dates Order
+
+SELECT 
+	*
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
