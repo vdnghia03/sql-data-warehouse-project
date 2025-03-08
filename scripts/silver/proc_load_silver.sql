b5e70edd70a509e738d2ad57605c06451ea640f3
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-08 14:29:42 +0700

    Insert Data to silver. crm_sales_details

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -76,7 +76,17 @@ FROM bronze.crm_prd_info
 -- WHERE REPLACE(SUBSTRING(TRIM(prd_key), 1, 5), '-', '_') NOT IN
 -- 	(SELECT DISTINCT id FROM bronze.erp_px_cat_g1v2)
 
-
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, sls_order_dt
+	, sls_ship_dt
+	, sls_due_dt
+	, sls_sales
+	, sls_quantity
+	, sls_price
+)
 SELECT
 	sls_ord_num
 	, sls_prd_key
@@ -85,17 +95,23 @@ SELECT
 		WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
 		ELSE CAST( CAST(sls_order_dt AS VARCHAR) AS DATE ) 
 	END AS sls_order_dt
-
 	, CASE 
 		WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
 		ELSE CAST( CAST(sls_ship_dt AS VARCHAR) AS DATE ) 
 	END AS sls_ship_dt
-
 	, CASE 
 		WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
 		ELSE CAST( CAST(sls_due_dt AS VARCHAR) AS DATE ) 
 	END AS sls_due_dt
-	, sls_sales
+	, CASE 
+		WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != ABS(sls_price) * sls_quantity
+				THEN ABS(sls_price) * sls_quantity
+		ELSE sls_sales
+	END AS sls_sales  -- Recaculate sales if original value is missing or incorrect
 	, sls_quantity
-	, sls_price
+	, CASE 
+			WHEN sls_price <= 0 OR sls_price IS NULL
+					THEN sls_sales / NULLIF(sls_quantity,0)
+			ELSE sls_price
+	END AS sls_price -- Derive price if original value is invalid
 FROM bronze.crm_sales_details
