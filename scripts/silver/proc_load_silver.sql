2be8fa7d92e63ea74bbf5428badf916aa573a04f
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-09 15:26:16 +0700

    Insert Data to silver. erp_px_cat_g1v2

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -155,3 +155,18 @@ SELECT DISTINCT
 		ELSE TRIM(cntry)
 	END AS cntry
 FROM bronze.erp_loc_a101
+
+--================
+--================
+INSERT INTO silver.erp_px_cat_g1v2(
+	id
+	, cat
+	, subcat
+	, maintenance
+)
+SELECT
+	id
+	, cat
+	, subcat
+	, maintenance
+FROM bronze.erp_px_cat_g1v2
