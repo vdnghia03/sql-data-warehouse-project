589e6ca62378df01f784140f008252ab7ee58e11
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-11 16:20:34 +0700

    Update ddl_gold.sql -- Create dim_products

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,3 +1,17 @@
+/*
+===================================================================
+Create DDL gold - Dimension and Fact
+===================================================================
+Script Purpose:
+
+
+
+
+===================================================================
+
+*/
+
+
 CREATE VIEW gold.dim_customers AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key -- Suggorate Key
@@ -18,3 +32,23 @@ LEFT JOIN silver.erp_cust_az12 AS ca
 ON		ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 AS la
 ON      ci.cst_key = la.cid
+
+--===================================
+--==================================
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key
+	, pn.prd_id AS product_id
+	, pn.prd_key AS product_number
+	, pn.prd_nm AS product_name
+	, pn.cat_id AS category_id
+	, pc.cat AS category
+	, pc.subcat AS subcategory
+	, pc.maintenance AS maintenance	
+	, pn.prd_cost AS cost
+	, pn.prd_line AS product_line
+	, pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 AS pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- Filter Out all historical Data
