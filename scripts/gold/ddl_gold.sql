509994b493a9619ddbbc5d9a6407611e71d93fed
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-12 22:33:34 +0700

    Update ddl_gold.sql -- Create Fact Sales

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -52,3 +52,24 @@ FROM silver.crm_prd_info AS pn
 LEFT JOIN silver.erp_px_cat_g1v2 AS pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL -- Filter Out all historical Data
+
+
+--========================================
+--=======================================
+CREATE VIEW gold.fact_sales AS
+SELECT
+	sd.sls_ord_num AS order_number
+	, pr.product_key
+	, cu.customer_key
+	, sd.sls_order_dt AS order_date
+	, sd.sls_ship_dt AS ship_date
+	, sd.sls_due_dt AS due_date
+	, sd.sls_sales AS sales_amount
+	, sd.sls_quantity AS quantity
+	, sd.sls_price AS price
+FROM silver.crm_sales_details AS sd
+LEFT JOIN gold.dim_products AS pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers AS cu
+ON sd.sls_cust_id = cu.customer_id
+
