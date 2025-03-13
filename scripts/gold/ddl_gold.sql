1fed86e26be0342bcdeea1041c30324bb0c14ec3
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-13 12:13:09 +0700

    Update ddl_gold.sql -- Update Format

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -21,23 +21,23 @@ Usage:
 CREATE VIEW gold.dim_customers AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key -- Suggorate Key
-	, ci.cst_id AS customer_id
-	, ci.cst_key AS customer_number
-	, ci.cst_firstname AS first_name
-	, ci.cst_lastname AS last_name
-	, la.cntry AS country
-	, ci.cst_marital_status AS marital_status
+	, ci.cst_id 			       AS customer_id
+	, ci.cst_key 			       AS customer_number
+	, ci.cst_firstname                     AS first_name
+	, ci.cst_lastname                      AS last_name
+	, la.cntry                             AS country
+	, ci.cst_marital_status                AS marital_status
 	, CASE 
 		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr 
 		ELSE COALESCE(ca.gen, 'n/a')
-	END AS gender -- CRM is the master for gender info - Data integration
-	, ca.bdate AS birthday
-	, ci.cst_create_date AS create_date
+	END 				       AS gender -- CRM is the master for gender info - Data integration
+	, ca.bdate                             AS birthday
+	, ci.cst_create_date                   AS create_date
 FROM silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS ca
-ON		ci.cst_key = ca.cid
+	ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 AS la
-ON      ci.cst_key = la.cid
+	ON ci.cst_key = la.cid
 
 
 -- =============================================================================
@@ -46,16 +46,16 @@ ON      ci.cst_key = la.cid
 CREATE VIEW gold.dim_products AS 
 SELECT
 	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key
-	, pn.prd_id AS product_id
-	, pn.prd_key AS product_number
-	, pn.prd_nm AS product_name
-	, pn.cat_id AS category_id
-	, pc.cat AS category
-	, pc.subcat AS subcategory
-	, pc.maintenance AS maintenance	
-	, pn.prd_cost AS cost
-	, pn.prd_line AS product_line
-	, pn.prd_start_dt AS start_date
+	, pn.prd_id 						AS product_id
+	, pn.prd_key 						AS product_number
+	, pn.prd_nm 						AS product_name
+	, pn.cat_id 						AS category_id
+	, pc.cat 						AS category
+	, pc.subcat 						AS subcategory
+	, pc.maintenance 					AS maintenance	
+	, pn.prd_cost 						AS cost
+	, pn.prd_line 						AS product_line
+	, pn.prd_start_dt 					AS start_date
 FROM silver.crm_prd_info AS pn
 LEFT JOIN silver.erp_px_cat_g1v2 AS pc
 ON pn.cat_id = pc.id
@@ -67,18 +67,18 @@ WHERE prd_end_dt IS NULL -- Filter Out all historical Data
 -- =============================================================================
 CREATE VIEW gold.fact_sales AS
 SELECT
-	sd.sls_ord_num AS order_number
+	sd.sls_ord_num 		AS order_number
 	, pr.product_key
 	, cu.customer_key
-	, sd.sls_order_dt AS order_date
-	, sd.sls_ship_dt AS ship_date
-	, sd.sls_due_dt AS due_date
-	, sd.sls_sales AS sales_amount
-	, sd.sls_quantity AS quantity
-	, sd.sls_price AS price
+	, sd.sls_order_dt 	AS order_date
+	, sd.sls_ship_dt 	AS ship_date
+	, sd.sls_due_dt 	AS due_date
+	, sd.sls_sales 		AS sales_amount
+	, sd.sls_quantity 	AS quantity
+	, sd.sls_price 		AS price
 FROM silver.crm_sales_details AS sd
 LEFT JOIN gold.dim_products AS pr
-ON sd.sls_prd_key = pr.product_number
+	ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers AS cu
-ON sd.sls_cust_id = cu.customer_id
+	ON sd.sls_cust_id = cu.customer_id
 
